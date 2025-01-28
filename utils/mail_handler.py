0274c3693d5c87509b29f5dd46d2eaa7c285a86f
Parampreet Singh <rubercuber.30@gmail.com>
2025-01-28 21:53:14 +0530

    feat: Add PDF attachment in mail

--- utils/mail_handler.py
+++ utils/mail_handler.py
@@ -1,3 +1,6 @@
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 import smtplib
 
 class EmailSender:
@@ -5,16 +8,36 @@ class EmailSender:
     def __init__(self, email, password):
         self.email = email
         self.__password = password
+        self._server = self.connect_to_server()
     
+    def connect_to_server(self):
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(self.email, self.__password)
+        return server
 
-    def __call__(self, receiver_email, subject, message):
-        with smtplib.SMTP('smtp.gmail.com', 587) as server:
-            server.starttls()
-            server.login(self.email, self.__password)
-            msg = f"Subject: {subject}\n\n{message}"
-            server.sendmail(self.email, receiver_email, msg)
+    def __call__(self, receiver_email, subject, message, pdf=None):
+        if not self._server:
+            self._server = self.connect_to_server()
+
+        msg = MIMEMultipart()
+        msg['From'] = self.email
+        msg['To'] = receiver_email
+        msg['Subject'] = subject
+        msg.attach(MIMEText(message, "plain"))
+        
+        if pdf:
+            with open(pdf, "rb") as attachment:
+                pdf_part = MIMEApplication(attachment.read(), _subtype="pdf")
+                pdf_part.add_header("Content-Disposition", f"attachment; filename= {os.path.basename(pdf)}")
+                msg.attach(pdf_part)
+        
+        try:
+            self._server.sendmail(self.email, receiver_email, msg.as_string())
             return 0
-        return 1
+        except Exception as e:
+            print(f"Error: {e}")
+            return 1
 
 
 if __name__ == "__main__":
@@ -22,7 +45,10 @@ if __name__ == "__main__":
     from dotenv import load_dotenv
     EMAIL = os.getenv("EMAIL")
     PASSWORD = os.getenv("PASSWORD")
+    MAIL_SUBJECT = os.getenv("HR_SUBJECT1")
+    MAIL_BODY = os.getenv("REFERRAL_BODY1")
+    print(EMAIL, PASSWORD, MAIL_SUBJECT, MAIL_BODY)
     load_dotenv()
 
     send_email = EmailSender(EMAIL, PASSWORD)
-    send_email("connectwithparam.30@gmail.com", "Test", "Hello, this is a test email")
+    send_email("mrintrovert.730@gmail.com", MAIL_SUBJECT, MAIL_BODY, pdf="Resume - Parampreet Singh.pdf")
